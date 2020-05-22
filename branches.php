f204425d0380a6beef32ce7904d948fd3565554e
Omar Ashraf <alexcruz2255@gmail.com>
2020-05-23 00:36:18 +0200

    psycologies page

---
+++ branches.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="layout/css/blogcss.css">
+        <title>Psychology branches</title>
+        
+    </head>
+    <body>
+        <div class="blog">
+            <h2>What is psychology?</h2>
+            <p class="paragraph" >
+            The mind is highly complex, and conditions that relate to it can be hard to treat.<br><br>
+Thought processes, emotions, memories, dreams, perceptions, and so on cannot be seen physically, like a skin rash or heart defect.<br><br>
+While physical signs of some mental health issues can be observed, such as the plaques that develop with Alzheimer’s disease, many theories of psychology are based on observation of human behavior.<br><br>
+A practicing psychologist will meet with patients, carry out assessments to find out what their concerns are and what is causing any difficulties, and recommend or provide treatment, for example, through counselling and psychotherapy.<br><br>
+Psychologists may have other roles, too. They may carry out studies to advise health authorities and other bodies on social and other strategies, assess children who find it difficult to learn in school, give workshops on how to prevent bullying, work with recruitment teams in companies, and much more.<br>
+            </p>
+        </div>
+        <hr>
+        <div class="blog">
+            <h2>Branches of psychology</h2>
+            <p class="paragraph" >
+            There are different types of psychology that serve different purposes. There is no fixed way of classifying them, but here are some common types.<br>
+            </p>
+        </div>
+        <div class="blog">
+            <h3>Clinical psychology</h3>
+            <p>
+            •	Clinical psychology integrates science, theory, and practice in order to understand, predict and relieve problems with adjustment, disability, and discomfort. It promotes adaption, adjustment, and personal development.<br>
+            </p>
+             <p>
+            •	A clinical psychologist concentrates on the intellectual, emotional, biological, psychological, social, and behavioral aspects of human performance throughout a person’s life, across varying cultures and socioeconomic levels.<br>
+            </p>
+             <p>
+           •	 Clinical psychology can help us to understand, prevent, and alleviate psychologically-caused distress or dysfunction, and promote an individual’s well-being and personal development.<br>
+            </p>
+             <p>
+           •	Psychological assessment and psychotherapy are central to the practice of clinical psychology, but clinical psychologists are often also involved in research, training, forensic testimony, and other areas.<br>
+            </p>
+            <br>
+        </div>
+         <div class="blog">
+            <h3>Cognitive psychology</h3>
+            <p>
+           •	Cognitive psychology investigates internal mental processes, such as problem solving, memory, learning, and language. It looks at how people think, perceive, communicate, remember, and learn. It is closely related to neuroscience, philosophy, and linguistics.<br>
+            </p>
+             <p>
+           •	Cognitive psychologists look at how people acquire, process, and store information.<br>
+            </p>
+             <p>
+           •	Practical applications include how to improve memory, increase the accuracy of decision-making, or how to set up educational programs to boost learning.<br>
+            </p>
+             <br>
+        </div>
+         <div class="blog">
+            <h3>Developmental psychology</h3>
+            <p>
+            •	This is the scientific study of systematic psychological changes that a person experiences over the life span, often referred to as human development.<br>
+            </p>
+             <p>
+            • It focuses not only on infants and young children but also teenagers, adults, and older people.<br>
+            </p>
+             <p>
+        	•	Factors include motor skills, problem solving, moral understanding, acquiring language, emotions, personality, self-concept, and identity formation.<br>
+            </p>
+             <p>
+        	•	It also looks at innate mental structures against learning through experience, or how a person’s characteristics interact with environmental factors and how this impacts development.<br>
+            </p>
+             <p>
+        	•	Developmental psychology overlaps with fields such as linguistics.<br>
+            </p>
+             <br>
+        </div>
+        <div class="blog">
+            <h3>Evolutionary psychology</h3>
+            <p>
+            •	Evolutionary psychology looks at how human behavior, for example language, has been affected by psychological adjustments during evolution.<br>
+            </p>
+             <p>
+            •	An evolutionary psychologist believes that many human psychological traits are adaptive in that they have enabled us to survive over thousands of years.<br>
+            </p>
+             <br>
+        </div>
+        <div class="blog">
+            <h3>Forensic psychology</h3>
+            <p>
+            •	Forensic psychology involves applying psychology to criminal investigation and the law.<br>
+            </p>
+             <p>
+            •	A forensic psychologist practices psychology as a science within the criminal justice system and civil courts.<br>
+            </p>
+             <p>
+        	•	It involves assessing the psychological factors that might influence a case or behavior and presenting the findings in court.<br>
+            </p>
+             <br>
+        </div>
+        <div class="blog">
+            <h3>Health psychology</h3>
+            <p>
+            •	Health psychology is also called behavioral medicine or medical psychology.<br>
+            </p>
+             <p>
+            •	It observes how behavior, biology, and social context influence illness and health.<br>
+            </p>
+             <p>
+        	•	A physician often looks first at the biological causes of a disease, but a health psychologist will focus on the whole person and what influences their health status. This may include their socioeconomic status, education, and background, and behaviors that may have an impact on the disease, such as compliance with instructions and medication.<br>
+            </p>
+             <p>
+        	•	Health psychologists usually work alongside other medical professionals in clinical settings.<br>
+            </p>
+             <br>
+        </div>
+        <div class="blog">
+            <h3>Neuropsychology</h3>
+            <p>
+            •   Neuropsychology looks at the structure and function of the brain in relation to behaviors and psychological processes. A neuropsychology may be involved if a condition involves lesions in the brain, and assessments that involve recording electrical activity in the brain.<br>
+            </p>
+             <p>
+            •	A neuropsychological evaluation is used to determine whether a person is likely to experience behavioral problems following suspected or diagnosed brain injury, such as a stroke.<br>
+            </p>
+             <p>
+        	•	The results can enable a doctor to provide treatment that may help the individual achieve possible improvements in cognitive damage that has occurred.<br>
+            </p>
+             <br>
+        </div>
+        <div class="blog">
+            <h3>Occupational psychology</h3>
+            <p>
+           •	Occupational or organizational psychologists are involved in assessing and making recommendations about the performance of people at work and in training.<br>
+            </p>
+             <p>
+           •	They help companies to find more effective ways to function, and to understand how people and groups behave at work.<br>
+            </p>
+             <p>
+        	•	This information can help improve effectiveness, efficiency, job satisfaction, and employee retention.<br>
+            </p>
+             <br>
+        </div>
+        <div class="blog">
+            <h3>Social psychology</h3>
+            <p>
+           •	Social psychology uses scientific methods to understand how social influences impact human behavior. It seeks to explain how feelings, behavior, and thoughts are influenced by the actual, imagined or implied presence of other people.<br>
+            </p>
+             <p>
+           •	A social psychologist looks at group behavior, social perception, non-verbal behavior, conformity, aggression, prejudice, and leadership. Social perception and social interaction are seen as key to understanding social behavior.<br>
+            </p>
+             <p class="paragraph">
+            Other branches include military, consumer, educational, cross-cultural, and environmental psychology. The number of branches continues to grow.
+            </p>
+             <br>
+        </div>
+    </body>
+
+</html>
